48343696ca7dbcb1e30194d5d50e2a0765548e34
Massimiliano Negretti <massimiliano.negretti@gmail.com>
2019-05-08 15:25:40 +0200

    add St7789 driver

--- display/display.mk
+++ display/display.mk
@@ -3,6 +3,7 @@ CSRCS += monitor.c
 CSRCS += R61581.c
 CSRCS += SSD1963.c
 CSRCS += ST7565.c
+CSRCS += ST7789.c
 
 DEPPATH += --dep-path $(LVGL_DIR)/lv_drivers/display
 VPATH += :$(LVGL_DIR)/lv_drivers/display
